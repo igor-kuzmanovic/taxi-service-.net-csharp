a7218e5c2c9da8c2158391391b584dd6acaa9418
igor.kuzmanovic.996 <igor.kuzmanovic.996@gmail.com>
2018-06-18 00:39:13 +0200

    Installed EntityFramework

---
+++ TaxiService/TaxiService/DAL/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TaxiService.DAL
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base("ApplicationContext") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) { }
+    }
+}
