6e5089ffeea74668a78d7de1360d5fef7aefba1d
igor.kuzmanovic.996 <igor.kuzmanovic.996@gmail.com>
2018-06-18 00:41:07 +0200

    Enabled migrations

---
+++ TaxiService/TaxiService/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace TaxiService.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TaxiService.DAL.ApplicationContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TaxiService.DAL.ApplicationContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
