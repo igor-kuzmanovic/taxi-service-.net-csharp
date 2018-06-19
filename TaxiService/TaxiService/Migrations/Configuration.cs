236335860e490dabeca766a40fae30b1acd96d28
igor.kuzmanovic.996 <igor.kuzmanovic.996@gmail.com>
2018-06-19 17:30:29 +0200

    Allowed data loss on migrations

--- TaxiService/TaxiService/Migrations/Configuration.cs
+++ TaxiService/TaxiService/Migrations/Configuration.cs
@@ -10,6 +10,7 @@ namespace TaxiService.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(TaxiService.DAL.ApplicationContext context)
