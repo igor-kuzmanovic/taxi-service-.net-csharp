8009777e7bfe1c55e08cb488d3236daf7ce89b4d
igor.kuzmanovic.996 <igor.kuzmanovic.996@gmail.com>
2018-09-01 05:06:31 +0200

    Removed all login logic

--- TaxiService/TaxiService/Controllers/LoginController.cs
+++ TaxiService/TaxiService/Controllers/LoginController.cs
@@ -10,64 +10,19 @@ namespace TaxiService.Controllers
 {
     public class LoginController : Controller
     {
-        private readonly AppDbContext DB { get; } = new AppDbContext();
-
         public ActionResult Index()
         {
-            var user = CheckUser();
-
-            ViewBag.User = user;
-            return View();
+            return null;
         }
 
-        [HttpPost]
         public ActionResult SignIn(SignInForm userForm)
         {
-            var user = CheckUser();
-
-            if (!ModelState.IsValid)
-            {
-                return View(userForm);
-            }
-
-            var findUser = DB.AppUsers.SingleOrDefault(u => u.Username == userForm.Username);
-
-            if (findUser == null || findUser.Password != userForm.Password)
-            {
-                ModelState.AddModelError("", "Invalid login attempt.");
-                return View(userForm);
-            }
-
-            user.Username = userForm.Username;
-            user.Password = userForm.Password;
-            user.IsLoggedIn = true;
-
-            ViewBag.User = user;
-            return View("Home");
+            return null;
         }
 
         public ActionResult SignOut()
         {
-            Session.Abandon();
-
-            var user = new AppUser();
-            Session["User"] = user;
-
-            ViewBag.User = user;
-            return View("Index");
-        }
-
-        private AppUser CheckUser()
-        {
-            var user = (AppUser)Session["User"];
-
-            if (user == null)
-            {
-                user = new AppUser();
-                Session["User"] = user;
-            }
-
-            return user;
+            return null;
         }
     }
 }
